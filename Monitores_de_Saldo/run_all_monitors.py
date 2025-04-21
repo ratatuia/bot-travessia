cb3a241bb505e58d106280e5d9d5e9b1754c4181
ratatuia <rafa85.lima@gmail.com>
2025-04-20 21:38:09 -0300

    fix path saldo_apis para Render

--- Monitores_de_Saldo/run_all_monitors.py
+++
@@ -1,6 +0,0 @@
-from Monitores_de_Saldo.openai_monitor import monitorar_openai
-from Monitores_de_Saldo.twilio_monitor import monitorar_twilio
-
-if __name__ == "__main__":
-    monitorar_openai()
-    monitorar_twilio()
--- saldo_apis/loop_diario.py
+++ saldo_apis/loop_diario.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import time
 import datetime
 from saldo_apis.openai_monitor import consultar_saldo
