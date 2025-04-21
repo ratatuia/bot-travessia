46f8dc8eb0fa85827559d2b2413fc586f8c7c9ad
ratatuia <rafa85.lima@gmail.com>
2025-04-21 12:52:12 -0300

    ajustes locais antes do pull

--- saldo_apis/run_all_monitors.py
+++ saldo_apis/run_all_monitors.py
@@ -1,6 +1,6 @@
-from Monitores_de_Saldo.openai_monitor import monitorar_openai
-from Monitores_de_Saldo.twilio_monitor import monitorar_twilio
+from saldo_apis.openai_monitor import consultar_saldo
+from saldo_apis.twilio_monitor import monitorar_twilio
 
 if __name__ == "__main__":
-    monitorar_openai()
+    consultar_saldo()
     monitorar_twilio()
